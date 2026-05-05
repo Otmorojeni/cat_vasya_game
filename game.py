e1bb53cef159b9e7fa2f46b4ce4595dd149fa923
mashtakov <mashtakov@gmail.com>
2026-05-05 20:48:16 +0300

    Реализован модуль 3. Добавлен абстрактный класс Spells c абстрактным методом cast() и унаследованными классами - Fireball, IceLance, LightningBolt. Доработан класс Unit, добавдены атрибуты: spells(список заклинаний персонажа) и mana(максимальное число маны для использования заклинаний). Добавлены методы: add_spell(spell) - позваляет добавить заклинание персонажу. cast_spell(index) - позволяет вызвать определенное заклинание с проверкой вызова и маны персонажа. В классе Character добавлен метод calculate_max_mana - в зависимости от класса персонажа вычисляет максимальное значение маны по формуле кота Василия

--- game.py
+++ game.py
@@ -13,6 +13,8 @@ class Unit(ABC):
         self.wisdom = wisdom
         self.intelligence = intelligence
         self.charisma = charisma
+        self.spells: list[Spell] = []
+        self.mana: float = 0
 
     @abstractmethod
     def calculate_max_health(self) -> int:
@@ -28,6 +30,21 @@ class Unit(ABC):
     def calculate_defense(self) -> int:
         """Абстрактный метод вычисления защиты"""
         pass
+
+    def add_spell(self, spell: Spell) -> None:
+        """Добавление заклинания персонажу"""
+        self.spells.append(spell)
+
+    def cast_spell(self, index: int) -> str:
+        """Вызов заклинания"""
+        if index >= len(self.spells) + 1:
+            return "Заклинание не найдено!"
+
+        if self.mana >= self.spells[index - 1].mana_cost:
+            self.mana -= self.spells[index - 1].mana_cost
+            return self.spells[index - 1].cast()
+        else:
+            return "Маны не хватает!"
 
 
 class Character(Unit):
@@ -45,6 +62,7 @@ class Character(Unit):
         self.max_health = self.calculate_max_health()
         self.damage = self.calculate_damage()
         self.defense = self.calculate_defense()
+        self.mana = self.calculate_max_mana()
 
     def calculate_max_health(self) -> int:
         """Вычисление максимального здоровья персонажа - телосложение * 10 + сила / 2"""
@@ -71,3 +89,61 @@ class Character(Unit):
             return int(self.dexterity * 1.6 + self.constitution / 5)
         else:
             return 0
+        
+    def calculate_max_mana(self) -> int:
+        """Вычисление показателя максимальной маны"""
+        if self.character_class == "warrior":
+            return int(self.intelligence + self.strength / 2)
+        elif self.character_class == "mage":
+            return int(self.intelligence * 3 + self.wisdom)
+        elif self.character_class == "hunter":
+            return int(self.dexterity * 1.5 + self.wisdom / 2)
+        else:
+            return 0
+
+
+class Spell(ABC):
+    """Абстрактный класс заклинаний персонажа"""
+
+    def __init__(self, name: str, damage: float, mana_cost: float) -> None:
+        self.name = name
+        self.damage = damage
+        self.mana_cost = mana_cost
+
+    @abstractmethod
+    def cast(self) -> str:
+        """Абстрактный метод эффекта заклинания"""
+        pass
+
+
+class Fireball(Spell):
+    """Заклинание Fireball"""
+
+    def __init__(self) -> None:
+        super().__init__(name="Fireball", damage=35, mana_cost=15)
+
+    def cast(self) -> str:
+        """Эффект заклинания"""
+        return f"-35HP"
+    
+
+class IceLance(Spell):
+    """Заклинание IceLance"""
+
+    def __init__(self) -> None:
+        super().__init__(name="IceLance", damage=25, mana_cost=10) 
+
+    def cast(self) -> str:
+        """Эффект заклинания"""
+        return f"-25HP"
+    
+
+class LightningBolt(Spell):
+    """Заклинание LightningBolt"""
+
+    def __init__(self) -> None:
+        super().__init__(name="LightningBolt", damage=40, mana_cost=20)
+
+    def cast(self) -> str:
+        """Эффект заклинания"""
+        return f"-40HP"
